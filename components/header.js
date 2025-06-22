4673e7b76325d7cc40d466bbbdc99b24c488542f
Epic Praxis <blackpearl0129@naver.com>
2025-06-23 02:39:33 +0900

    Update header.js

--- components/header.js
+++ components/header.js
@@ -28,7 +28,7 @@ class Header extends HTMLElement {
                 <nav>
                     <ul class="main-menu">
                       <li><a href="index.html#curriculum" class="${this.isActive('#')}">커리큘럼</a></li>
-                      <li><a href="index.html#roadmap">학습 로드맵</a></li>
+                      <li><a href="booster.html">썸머부스터</a></li>
                       <li><a href="campus.html">캠퍼스 찾기</a></li>
                       <li><a href="https://tr.ee/blog-epiclang" rel="noopener noreferrer">
                         <span style="display: inline-flex; align-items: center; gap: 3px;">블로그 
