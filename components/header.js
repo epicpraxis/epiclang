e7585574d62eac1b6559301e48fa0a3dc9f1bc3e
Epic Praxis <blackpearl0129@naver.com>
2026-04-14 21:26:47 +0900

    Update summer booster link from 2025 to 2026

--- components/header.js
+++ components/header.js
@@ -27,7 +27,7 @@ class Header extends HTMLElement {
                 </div>
                 <nav>
                     <ul class="main-menu">
-                      <li><a href="booster.html" class="${this.isActive('#')}">🌴 2025 썸머부스터</a></li>|
+                      <li><a href="booster.html" class="${this.isActive('#')}">🌴 2026 썸머부스터</a></li>|
                       <li><a href="index.html#concerns">정규 프로그램</a></li>
                       <li><a href="campus.html">캠퍼스 찾기</a></li>
                       <li><a href="https://tr.ee/blog-epiclang" rel="noopener noreferrer">
